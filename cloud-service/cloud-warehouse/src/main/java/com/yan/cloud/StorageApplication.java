759f77191ba70a514fc63daecfa733fbb2d37af3
Administrator <289572111@qq.com>
2020-04-27 10:42:32 +0800

    统一配置

--- cloud-service/cloud-warehouse/src/main/java/com/yan/cloud/StorageApplication.java
+++ cloud-service/cloud-warehouse/src/main/java/com/yan/cloud/StorageApplication.java
@@ -1,12 +1,13 @@
 package com.yan.cloud;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DruidDataSourceAutoConfigure.class})
 @EnableDiscoveryClient
 @MapperScan("com.yan.cloud.dao")
 public class StorageApplication {

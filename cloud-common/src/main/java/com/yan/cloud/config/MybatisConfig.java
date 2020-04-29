4c95600ab77a1718e6b76cdb598b9670dff0e3ff
Administrator <289572111@qq.com>
2020-04-29 14:20:25 +0800

    整合ShardingJDBC--未完成

--- cloud-common/src/main/java/com/yan/cloud/config/MybatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.yan.cloud.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan({"com.yan.cloud.dao"})
-public class MybatisConfig {
-}

2920ee01615f2d3593d737c4414e9274ecdd10e2
Administrator <289572111@qq.com>
2020-05-21 16:01:38 +0800

    excel工具

---
+++ cloud-common/src/main/java/com/yan/cloud/util/ExcelDemo.java
@@ -0,0 +1,14 @@
+package com.yan.cloud.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExcelDemo {
+    private String name;
+    private Integer age;
+    private Integer sex;
+}
